ee05b4dd3e4a4ddd24587164578bba34ea8e8576
d-val-r <dvrudenya@noctrl.edu>
2021-07-10 11:18:43 -0500

    added comments

--- hash_extract.py
+++ hash_extract.py
@@ -1,7 +1,12 @@
 file = open("shuffled.csv")
 
+# the input file is a CSV whose first entry is the hash of the block
 for line in file:
     sep = line.split(",")
+
+    # instead of writing directly to a file from this script, it is used
+    # with an output redirect (> or >>) in Linux to write the file 
+    # (eg. python hash_extract.py >> filename)
     print(sep[0])
 
 file.close()
