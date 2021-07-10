bae25b8c3ecf5dad2172f9a63866a2e48ac2a2ae
d-val-r <dvrudenya@noctrl.edu>
2021-07-10 11:26:24 -0500

    added comments

--- shuffler.sh
+++ shuffler.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-count=0
-var=`python parser.py`
-while (($var == 0)); do
-	echo "count is $count"
+
+# save the output of running the parser.py script to a variable
+parse_output=`python parser.py`
+
+# whlie the parse_output is 0, no malicious data was found, so reshuffle the
+# input
+while (($parse_output == 0)); do
+
+	# bash command to shuffle 5000 lines of the input data and output it
+	# to a CSV
 	shuf -n 5000 pre_shuffled.txt > shuffled.csv
-	var=`python parser.py`
-	((count++))
+
+	# re-run the parser and update the parse_output variable
+	parse_output=`python parser.py`
 done
 
-echo $var
+
+# output the amount of malicious transactions found
+echo $parse_output
 
