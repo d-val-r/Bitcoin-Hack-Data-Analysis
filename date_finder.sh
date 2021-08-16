ff0b039964436cf731fc20c911c8035192b8f516
d-val-r <dvrudenya@noctrl.edu>
2021-08-16 15:13:56 -0500

    update comments

--- date_finder.sh
+++ date_finder.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# read input from first argument to the script, a file of hashes for 
-# transactions that were confirmed to be malicious
+# the input file is a list of malicious hashes
 while IFS= read -r line; do
-	# using awk with a comma for a field separator, split each line of
-	# the file into columns and print the first and third columns, which
-	# is the hash of the transaction and the date/time it occured, then search for
-	# malicious lines and print only the unique ones
+	# output the INPUT hash and its date and timestamp, grep for only malicious
+	# hash outputs, and ensure there are no duplicates. Outputs to dates.txt
 	output=`cat btc_tx_2011_2013.csv | awk -F "," '{print $1, $3}' | grep $line | uniq`
 	echo $output
 done < $1
