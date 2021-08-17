1628e51cb6f7d8a72bec87c11d163a4d4414a007
d-val-r <dvrudenya@noctrl.edu>
2021-08-16 20:33:58 -0500

    testing

--- shuffler_2.sh
+++ shuffler_2.sh
@@ -6,5 +6,10 @@
 output=`ls -al data_files/20*.csv | awk '{print $9}' | awk -F "/" '{print $2}' | awk -F "." '{print $1}'`
 
 for line in $output; do
-	echo $line
+	
+	filename=`ls -al data_files/ | grep $line | awk '{print $9}'`
+	echo $filename
+	#cat data_files/dates.txt | grep $line | awk '{print $1}' >> $file
+
 done;
+
