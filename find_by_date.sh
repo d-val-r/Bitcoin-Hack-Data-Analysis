04e5afa1c339c5a08d70e5281fb494f398379389
d-val-r <dvrudenya@noctrl.edu>
2021-08-16 20:33:05 -0500

    update comments

--- find_by_date.sh
+++ find_by_date.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# input file is a list of unique dates at which maclicious hashes occur
+# input file is a list of unique dates (month/year) at which maclicious hashes occur 
 while IFS= read -r line; do
 	# format of the cat file is:
 	# input_hash,output_hash,datetime,amount_bitcoins
