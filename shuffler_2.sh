141c47e3920f0a9b2355dc248a05a37f5cf32fef
d-val-r <dvrudenya@noctrl.edu>
2021-08-20 17:38:49 -0500

    update filepaths and comments

--- shuffler_2.sh
+++ shuffler_2.sh
@@ -6,20 +6,20 @@
 # all csvs in the data_files folder start with 20 (for the year); pipe the 
 # output through awk several times to help format the output; the result is a 
 # list of dates (year/month) 
-output=`ls -al data_files/20*.csv | awk '{print $9}' | awk -F "/" '{print $2}' | awk -F "." '{print $1}'`
+output=`ls -al data_files/csv_files/20*.csv | awk '{print $9}' | awk -F "/" '{print $3}'| awk -F "." '{print $1}'`
 
 for line in $output; do
 	
 	# store just the filename
-	filename=`ls -al data_files/ | grep $line | awk '{print $9}'`
+	filename=`ls -al data_files/csv_files/ | grep $line | awk '{print $9}'`
 	
 	# shuffle 227 random lines from the file and print them into a new
-	# file named "{date}_hashes.txt"
-	shuf -n 227 data_files/$filename >> data_files/$line\_hashes.txt
+	# file named "{date}_hashes.csv"
+	shuf -n 227 data_files/csv_files/$filename >> data_files/csv_files/$line\_hashes.csv
 
-	# find malicious hashes from the dates.txt file and append them to the 
-	# file just created
-	cat data_files/dates.txt | grep $line | awk '{print $1}' >> data_files/$line\_hashes.txt
+	# find malicious hashes and dates from the below file and append them
+	# to the file just created; this will later be sorted by date
+	cat data_files/csv_files/malicious_hashes_and_dates.csv | grep $line >> data_files/csv_files/$line\_hashes.csv
 
 done;
 
