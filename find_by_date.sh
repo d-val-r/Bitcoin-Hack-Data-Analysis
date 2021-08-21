d6e30c6a8cd1553a6cbb4c596d90cfd133f8b417
d-val-r <dvrudenya@noctrl.edu>
2021-08-21 14:26:54 -0500

    change output format and filepath name

--- find_by_date.sh
+++ find_by_date.sh
@@ -5,8 +5,8 @@ while IFS= read -r line; do
 	# format of the cat file is:
 	# input_hash,output_hash,datetime,amount_bitcoins
 
-	# format of the output is "hash,datestamp"
-	cat data_files/clean_btc_tx_2011_2013.csv | grep $line | awk -F "," '{print $1, $3}' | awk '{print ($1"," $2)}' >> $line.csv
+	# format of the output is "hash,date timestamp"
+	cat data_files/csv_files/clean_btc_tx_2011_2013.csv | grep $line | awk -F "," '{print $1, $3}' | awk '{print $1"," $2, $3}' >> $line.csv
 
 
 done < $1
