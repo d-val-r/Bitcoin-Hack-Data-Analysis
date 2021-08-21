4352241105ae537db2d3a38174f83c1170c13e73
d-val-r <dvrudenya@noctrl.edu>
2021-08-21 13:09:13 -0500

    update filepath

--- date_finder.sh
+++ date_finder.sh
@@ -4,6 +4,6 @@
 while IFS= read -r line; do
 	# output the INPUT hash and its date and timestamp, grep for only malicious
 	# hash outputs, and ensure there are no duplicates. Outputs to dates.txt
-	output=`cat btc_tx_2011_2013.csv | awk -F "," '{print $1, $3}' | grep $line | uniq`
+	output=`cat data_files/csv_files/btc_tx_2011_2013.csv | awk -F "," '{print $1, $3}' | grep $line | uniq`
 	echo $output
 done < $1
